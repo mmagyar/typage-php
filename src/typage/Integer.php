d98a3788445a020bf043a918d6e36180869bcfc6
Mate Magyar <dev@mmagyar.com>
2016-01-01 19:06:53 +0100

    enable non strict checking for Integer type

--- src/typage/Integer.php
+++ src/typage/Integer.php
@@ -9,7 +9,8 @@ final class Integer extends Number {
     protected $typeName = "Integer";
 
     final public function subTypeCheck($value, $variableName, $soft) {
-        if (!is_int($value))
+        //Type coercion works here, because at this point we already made sure that what we have is a number, even if it's in a string
+        if ((static::$strictNumberCheck && !is_int($value)) || (!static::$strictNumberCheck && !is_int($value + 0)))
             return static::handleTypeError($this->getTypeString(), $value, $variableName, $soft);
 
         return intval($value);
